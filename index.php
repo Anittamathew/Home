af73eda1ce5e37f4648ca5e1c2e564420c113515
Vinayak030 <77478951+Vinayak030@users.noreply.github.com>
2022-03-04 09:53:46 +0530

    First page index

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<title>Home: Your Building Partner</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="w3school.css">
+<!-- <link rel="stylesheet" href="style.css"> -->
+
+<body>
+
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+    <a href="#home" class="w3-bar-item w3-button"><b>Home Safe Home </b></a>
+    <!-- Float links to the right. Hide them on small screens -->
+    <div class="w3-right w3-hide-small">
+      <a href="registerform.php" class="w3-bar-item w3-button">Register</a>
+      <a href="loginform.php" class="w3-bar-item w3-button">Login</a>
+      <!-- <a href="#contact" class="w3-bar-item w3-button">Contact</a> -->
+    </div>
+  </div>
+</div>
+
+<!-- Header -->
+<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+
+
+
+
+
+
+  <img class="w3-image" src="images/999.jpg" alt="Architecture" width="1500" height="800">
+  <div class="w3-display-middle w3-margin-top w3-center">
+    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>Home</b></span> <span class="w3-hide-small w3-text-light-grey">Building Partner</span></h1>
+  </div>
+
+
+</header>
+
+<!-- Page content -->
+<div class="w3-content w3-padding" style="max-width:1564px">
+
+  <!-- Project Section -->
+  <div class="w3-container w3-padding-32" id="projects">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Projects</h3>
+  </div>
+
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Traditional</div>
+        <img src="images/traditional.jpg" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Modern</div>
+        <img src="images/house2.jpg" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Renovated</div>
+        <img src="images/renovated.jpg" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Others</div>
+        <img src="images/others.jpg" alt="House" style="width:100%">
+      </div>
+    </div>
+  </div>
+<!--
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Summer House</div>
+        <img src="images/house2.jpg" alt="House" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Brick House</div>
+        <img src="images/house5.jpg" alt="House" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Renovated</div>
+        <img src="images/house4.jpg" alt="House" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Barn House</div>
+        <img src="images/house3.jpg" alt="House" style="width:99%">
+      </div>
+    </div>
+  </div>
+-->
+
+  
+  <div class="w3-container w3-padding-32" id="about">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">About</h3>
+    <p>Home is a new innovative website that aim to provide assistamnce to the general public who are interested in building their constructions in a new and innovative manner...We provide almost all essential factors of production and helps our clients to build the dream home effortlessly. 
+    </p>
+  </div>
+<!--
+  <div class="w3-row-padding w3-grayscale">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="images/pro.jpg" alt="John" style="width:50%">
+      <h3>Sarath Sagar</h3>
+      <p class="w3-opacity">Team Member</p>
+      <p>He is one of the team members guided the entire web development team...monitor team activities..trobleshoot problems..design and testing...</p>
+       <p><button class="w3-button w3-light-grey w3-block">Contact</button></p> 
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="images/pro.jpg" alt="Jane" style="width:50%">
+      <h3>Mohammed Shabab</h3>
+      <p class="w3-opacity">Team Member</p>
+      <p>He is one of the team members guided the entire web development team...monitor team activities..trobleshoot problems..design and testing...</p>
+       <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="images/pro.jpg" alt="Mike" style="width:50%">
+      <h3>Anjupriya K S</h3>
+      <p class="w3-opacity">Team Member</p>
+      <p>She is one of the team members guided the entire web development team...monitor team activities..trobleshoot problems..design and testing...</p>
+     <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="images/pro.jpg" alt="Dan" style="width:50%">
+      <h3>Shobhi K K</h3>
+      <p class="w3-opacity">Team Member</p>
+      <p>She is one of the team members guided the entire web development team...monitor team activities..trobleshoot problems..design and testing...</p>
+       <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+  </div>
+-->
+  <!-- Contact Section -->
+
+    <!--<form action="/action_page.php" target="_blank">
+      <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Email" required name="Email">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Subject" required name="Subject">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Comment" required name="Comment">
+      <button class="w3-button w3-black w3-section" type="submit">
+        <i class="fa fa-paper-plane"></i> SEND MESSAGE
+      </button>
+    </form>
+  </div>
+-->
+  
+<!-- Image of location/map -->
+<div class="w3-container">
+  <!-- <img src="images/map.jpg" class="w3-image" style="width:100%"> -->
+</div>
+<!-- End page content -->
+</div>
+
+
+<!-- Footer -->
+<footer class="w3-center w3-black w3-padding-16">
+  <p>Powered by <a href="index.php" title="W3.CSS" target="_blank" class="w3-hover-text-green">Home</a> Your Building Partner </p>
+</footer>
+
+</body>
+</html>
